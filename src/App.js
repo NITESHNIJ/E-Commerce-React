d8ef23dd5a7f7886882ac04ac4b63868358e6531
nitesh_nij <niteshnijhawan100@gmail.com>
2021-01-22 02:39:25 +0530

    adding personal project to github, for skills display

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import {Route} from './Routes';
+
+class App extends Component
+{
+
+
+
+
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React,{Component} from 'react';
+import Routes, {Route} from './Routes';
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<Routes/>,document.getElementById('root'));
